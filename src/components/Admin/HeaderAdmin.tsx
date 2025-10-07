8c3cc0aeaa08350e58ccaaa6847492ec98c74fa0
Le-Thi-Truc-Ha <hale071204@gmail.com>
2025-10-07 11:30:40 +0700

    First commit

--- src/components/Admin/HeaderAdmin.tsx
+++ src/components/Admin/HeaderAdmin.tsx
@@ -34,7 +34,7 @@ const HeaderAdmin = (): JSX.Element => {
         if (path == "") {  // Trang chủ
             setIndexOfItem(1);
         } else if (path == "/admin/product") {  // Sản phẩm
-            setIndexOfItem(9);
+            setIndexOfItem(2);
         } else if (path == "") {  // Đơn hàng
             setIndexOfItem(3);
         } else if (path == "") {  // Khách hàng
